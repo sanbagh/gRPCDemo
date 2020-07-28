8e963124acc2903ddef3c6399de74a7568ab58e5
sanjeev <sanjeevsanjeevbaghel@gmail.com>
2020-07-28 22:59:36 +0530

    GRPC setup

---
+++ Client/Program.cs
@@ -0,0 +1,20 @@
+﻿using Grpc.Core;
+using System;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Channel channel = new Channel("localhost:5000", ChannelCredentials.Insecure);
+            channel.ConnectAsync().ContinueWith((task) => { 
+                if(task.Status == System.Threading.Tasks.TaskStatus.RanToCompletion)
+                {
+                    Console.WriteLine("Connected to server..");
+                }
+            });
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Grpc.Core;
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Grpc.Core.Server server = new Grpc.Core.Server { Ports = { new ServerPort("localhost", 5000, ServerCredentials.Insecure) } };
+            server.Start();
+            Console.WriteLine("server started...");
+            Console.ReadLine();
+        }
+    }
+}
